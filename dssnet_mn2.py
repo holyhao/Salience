3c91c55aac786d8ee0084a629dda501fdad63632
holyhao <546730148@qq.com>
2018-10-05 22:39:40 +0800

    Add files via upload

---
+++ dssnet_mn2.py
@@ -0,0 +1,355 @@
+
+from collections import OrderedDict
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import init
+
+# vgg choice
+base = {'dss': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']}
+# extend vgg choice --- follow the paper, you can change it
+extra = {'dss': [(32, 32, 3, [8, 16, 32, 64]), (24, 24, 3, [4, 8, 16, 32]), (32, 32, 5, [8, 16]),
+                 (96, 96, 5, [4, 8]), (160, 160, 5, []), (320, 160, 7, [])]}
+connect = {'dss': [[2, 3, 4, 5], [2, 3, 4, 5], [4, 5], [4, 5], [], []]}
+
+# MoblieNetv2
+
+
+def _make_divisible(v, divisor, min_value=None):
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    :param v:
+    :param divisor:
+    :param min_value:
+    :return:
+    """
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+
+class LinearBottleneck(nn.Module):
+    def __init__(self, inplanes, outplanes, stride=1, t=6, activation=nn.ReLU6):
+        super(LinearBottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, inplanes * t, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(inplanes * t)
+        self.conv2 = nn.Conv2d(inplanes * t, inplanes * t, kernel_size=3, stride=stride, padding=1, bias=False,
+                               groups=inplanes * t)
+        self.bn2 = nn.BatchNorm2d(inplanes * t)
+        self.conv3 = nn.Conv2d(inplanes * t, outplanes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(outplanes)
+        self.activation = activation(inplace=True)
+        self.stride = stride
+        self.t = t
+        self.inplanes = inplanes
+        self.outplanes = outplanes
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.activation(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.activation(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.stride == 1 and self.inplanes == self.outplanes:
+            out += residual
+
+        return out
+
+
+class MobileNet2(nn.Module):
+    """MobileNet2 implementation.
+    """
+
+    def __init__(self, scale=1.0, input_size=224, t=6, in_channels=3, num_classes=1000, activation=nn.ReLU6):
+        """
+        MobileNet2 constructor.
+        :param in_channels: (int, optional): number of channels in the input tensor.
+                Default is 3 for RGB image inputs.
+        :param input_size:
+        :param num_classes: number of classes to predict. Default
+                is 1000 for ImageNet.
+        :param scale:
+        :param t:
+        :param activation:
+        """
+
+        super(MobileNet2, self).__init__()
+
+        self.scale = scale
+        self.t = t
+        self.activation_type = activation
+        self.activation = activation(inplace=True)
+        self.num_classes = num_classes
+
+        self.num_of_channels = [32, 16, 24, 32, 64, 96, 160, 320]
+        # assert (input_size % 32 == 0)
+
+        self.c = [_make_divisible(ch * self.scale, 8) for ch in self.num_of_channels]
+        self.n = [1, 1, 2, 3, 4, 3, 3, 1]
+        self.s = [2, 1, 2, 2, 2, 1, 2, 1]
+        self.conv1 = nn.Conv2d(in_channels, self.c[0], kernel_size=3, bias=False, stride=self.s[0], padding=1)
+        self.bn1 = nn.BatchNorm2d(self.c[0])
+        self.bottleneck_s = self._make_bottlenecks()
+        self.bottlenecks = nn.Sequential(self.bottleneck_s)
+
+        # Last convolution has 1280 output channels for scale <= 1
+        self.last_conv_out_ch = 1280 if self.scale <= 1 else _make_divisible(1280 * self.scale, 8)
+        self.conv_last = nn.Conv2d(self.c[-1], self.last_conv_out_ch, kernel_size=1, bias=False)
+        self.bn_last = nn.BatchNorm2d(self.last_conv_out_ch)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.dropout = nn.Dropout(p=0.2, inplace=True)  # confirmed by paper authors
+        self.fc = nn.Linear(self.last_conv_out_ch, self.num_classes)
+        self.init_params()
+
+    def init_params(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def _make_stage(self, inplanes, outplanes, n, stride, t, stage):
+        modules = OrderedDict()
+        stage_name = "LinearBottleneck{}".format(stage)
+
+        # First module is the only one utilizing stride
+        first_module = LinearBottleneck(inplanes=inplanes, outplanes=outplanes, stride=stride, t=t,
+                                        activation=self.activation_type)
+        modules[stage_name + "_0"] = first_module
+
+        # add more LinearBottleneck depending on number of repeats
+        for i in range(n - 1):
+            name = stage_name + "_{}".format(i + 1)
+            module = LinearBottleneck(inplanes=outplanes, outplanes=outplanes, stride=1, t=6,
+                                      activation=self.activation_type)
+            modules[name] = module
+
+        return nn.Sequential(modules)
+
+    def _make_bottlenecks(self):
+        modules = OrderedDict()
+        stage_name = "Bottlenecks"
+
+        # First module is the only one with t=1
+        bottleneck1 = self._make_stage(inplanes=self.c[0], outplanes=self.c[1], n=self.n[1], stride=self.s[1], t=1,
+                                       stage=0)
+        modules[stage_name + "_0"] = bottleneck1
+
+        # add more LinearBottleneck depending on number of repeats
+        for i in range(1, len(self.c) - 1):
+            name = stage_name + "_{}".format(i)
+            module = self._make_stage(inplanes=self.c[i], outplanes=self.c[i + 1], n=self.n[i + 1],
+                                      stride=self.s[i + 1],
+                                      t=self.t, stage=i)
+            modules[name] = module
+
+        return modules
+
+    def _make_bottlenecks_list(self):
+        modules = []
+        stage_name = "Bottlenecks"
+
+        # First module is the only one with t=1
+        bottleneck1 = self._make_stage(inplanes=self.c[0], outplanes=self.c[1], n=self.n[1], stride=self.s[1], t=1,
+                                       stage=0)
+        self.Bottlenecks_0 = bottleneck1
+        modules.append(self.Bottlenecks_0)
+
+        # add more LinearBottleneck depending on number of repeats
+        for i in range(1, len(self.c) - 1):
+            name = stage_name + "_{}".format(i)
+            foo = "self." + name
+            module = self._make_stage(inplanes=self.c[i], outplanes=self.c[i + 1], n=self.n[i + 1],
+                                      stride=self.s[i + 1],
+                                      t=self.t, stage=i)
+            exec(foo + " = module")
+            modules.append(eval(foo))
+
+        return modules
+
+    def get_layers(self):
+        out = OrderedDict()
+        out["conv1"] = self.conv1
+        out["bn1"] = self.bn1
+        out["activation"] = self.activation
+        for k in self.bottleneck_s.keys():
+            out[k] = self.bottleneck_s[k]
+        # out['conv_last'] = self.conv_last
+        # out['bn_last'] = self.bn_last
+        # out['avgpool'] = self.avgpool
+        return out
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activation(x)
+
+        x = self.bottlenecks(x)
+        x = self.conv_last(x)
+        x = self.bn_last(x)
+        x = self.activation(x)
+
+        # average pooling layer
+        x = self.avgpool(x)
+        x = self.dropout(x)
+
+        # flatten for input to fully-connected layer
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return F.log_softmax(x, dim=1) #TODO not needed(?)
+
+
+# vgg16
+
+
+def vgg(cfg, i, batch_norm=False):
+    layers = []
+    in_channels = i
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return layers
+
+
+# feature map before sigmoid: build the connection and deconvolution
+class ConcatLayer(nn.Module):
+    def __init__(self, list_k, k, scale=True):
+        super(ConcatLayer, self).__init__()
+        l, up, self.scale = len(list_k), [], scale
+        for i in range(l):
+            up.append(nn.ConvTranspose2d(1, 1, list_k[i], list_k[i] // 2, list_k[i] // 4))
+        self.upconv = nn.ModuleList(up)
+        self.conv = nn.Conv2d(l + 1, 1, 1, 1)
+        self.deconv = nn.ConvTranspose2d(1, 1, k * 2, k, k // 2) if scale else None
+        self.last_deconv = nn.ConvTranspose2d(1, 1, 2*2, 2, 1)
+
+    def forward(self, x, list_x):
+        elem_x = [x]
+        for i, elem in enumerate(list_x):
+            elem_x.append(self.upconv[i](elem))
+        if self.scale:
+            out = self.deconv(self.conv(torch.cat(elem_x, dim=1)))
+        else:
+            out = self.conv(torch.cat(elem_x, dim=1))
+        out = self.last_deconv(out)
+        return out
+
+
+# extend vgg: side outputs
+class FeatLayer(nn.Module):
+    def __init__(self, in_channel, channel, k):
+        super(FeatLayer, self).__init__()
+        self.main = nn.Sequential(nn.Conv2d(in_channel, channel, k, 1, k // 2), nn.ReLU(inplace=True),
+                                  # nn.Conv2d(channel, channel, k, 1, k // 2), nn.ReLU(inplace=True),
+                                  nn.Conv2d(channel, 1, 1, 1))
+
+    def forward(self, x):
+        return self.main(x)
+
+
+# extra part
+def extra_layer(vgg, cfg):
+    feat_layers, concat_layers, scale = [], [], 1
+    for k, v in enumerate(cfg):
+        # side output (paper: figure 3)
+        feat_layers += [FeatLayer(v[0], v[1], v[2])]
+        # feature map before sigmoid
+        concat_layers += [ConcatLayer(v[3], scale, k != 0)]
+        scale *= 2
+    return vgg, feat_layers, concat_layers
+
+
+# DSS network
+class DSS(nn.Module):
+    def __init__(self, base, feat_layers, concat_layers, connect):
+        super(DSS, self).__init__()
+        self.extract = [3, 5, 6, 8, 9]
+        self.connect = connect
+        self.base = nn.ModuleDict(base)
+        self.feat = nn.ModuleList(feat_layers)
+        self.comb = nn.ModuleList(concat_layers)
+        self.maxpool = nn.MaxPool2d(3, 2, 1)
+        self.pool = nn.AvgPool2d(3, 1, 1)
+
+    def forward(self, x, label=None):
+        prob, back, y, num, count = list(), list(), list(), 0, 1
+        for k in self.base.keys():
+            x = self.base[k](x)
+            if count in self.extract:
+                y.append(self.feat[num](x))
+                num += 1
+            count += 1
+        # side output
+        y.append(self.feat[num](self.maxpool(x)))
+        for i, k in enumerate(range(len(y))):
+            back.append(self.comb[i](y[i], [y[j] for j in self.connect[i]]))
+        # fusion map
+        back.append(torch.cat(back, dim=1).mean(dim=1, keepdim=True))
+        # add sigmoid
+        for i in back: prob.append(torch.sigmoid(i))
+        return prob
+
+
+# build the whole network
+def build_model():
+    return DSS(*extra_layer(MobileNet2().get_layers(), extra['dss']), connect['dss'])
+
+
+# weight init
+def xavier(param):
+    init.xavier_uniform_(param)
+
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d):
+        xavier(m.weight.data)
+    elif isinstance(m, nn.BatchNorm2d):
+        init.constant_(m.weight, 1)
+        init.constant_(m.bias, 0)
+
+
+if __name__ == '__main__':
+
+    def print_network(model, name):
+        num_params = 0
+        for p in model.parameters():
+            num_params += p.numel()
+        print(name)
+        print(model)
+        print("The number of parameters: {}".format(num_params))
+
+    net = build_model()
+    print_network(net, "orignal")
+    img = torch.randn(1, 3, 256, 256)
+    out = net(img)
+    k = [out[x] for x in [1, 2, 3, 6]]
+    print(len(out))
